570ffa4b25336f0bbe2e299644dbe92c9a7d5b84
Roona <roona.sa@gmail.com>
2016-03-23 00:55:59 -0400

    javakey

--- Projects/Test/src/com/roona/annotations/world.java
+++ Projects/Test/src/com/roona/annotations/world.java
@@ -1,21 +1,30 @@
 package com.roona.annotations;
 class shape
 {
-	void circleimpl()
+	void area(int a)
 	{
 		System.out.println("shape");
 	}
 }
 class circle extends shape
 {
-	void circleimpl()
+	int c;
+	int radius;
+	
+	void circleimpl(int r)
 	{
+		
 		System.out.println("circle");
+		c=r*r;
+		
+		
 	}
 }
 public class world {
 	public static void main(String[] args) {
 		shape s= new circle();
+		s.circleimpl();
+		
 	}
 
 }
