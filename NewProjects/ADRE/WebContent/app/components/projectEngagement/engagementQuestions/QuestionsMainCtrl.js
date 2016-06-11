7123dce171e37a763206026f1451db429c917c2a
Roona <roona.sa@gmail.com>
2016-06-11 00:34:37 -0500

    adre

--- NewProjects/ADRE/WebContent/app/components/projectEngagement/engagementQuestions/QuestionsMainCtrl.js
+++ NewProjects/ADRE/WebContent/app/components/projectEngagement/engagementQuestions/QuestionsMainCtrl.js
@@ -27,8 +27,53 @@
         }
         
         selectEngagementQuestion();
-        
         self.processChangeButton = function () {
+            var goCapacity = false;
+            for(var q in self.questions) {
+                self.questions[q].disabled = false;
+            }
+            //trigger capacity question route
+           
+            if(self.questions) {
+                if (self.questions[2].value || self.questions[4].value || self.questions[5].value) {
+                    goCapacity = true;
+                    self.questions[9].disabled = true;
+                    self.questions[10].disabled = true;
+                } if(self.questions[9].value){
+                    for(var q in self.questions) {
+                        if(q != 9){
+                            self.questions[q].disabled = true;
+                        }
+                    }
+                } if(self.questions[10].value){
+                    for(var q in self.questions) {
+                        if(q != 10){
+                            self.questions[q].disabled = true;
+                        }
+                    }
+                } if (self.questions[0].value || self.questions[1].value || self.questions[3].value || self.questions[6].value || self.questions[7].value || self.questions[8].value) {
+                    self.questions[9].disabled = true;
+                    self.questions[10].disabled = true;
+                } else if (self.questions[10].value) {
+                    //POC
+                   
+                }
+            }
+            //Engagement.questions
+            self.Engagement.capacityEngagement = goCapacity;
+           
+            //update stored questions
+            self.Engagement.questions = [];
+            for(var q in self.questions) {
+                if(self.questions[q].value == true) {
+                    Engagement.questions.push(self.questions[q].key);
+                }
+            }
+            
+            $scope.$parent.$parent.tabs[1].visible = goCapacity;
+            
+        };
+       /* self.processChangeButton = function () {
         	
             var goCapacity = false;
             //trigger capacity question route
@@ -54,7 +99,7 @@
                     Engagement.tempQuestions.push(self.questions[q].key);
                 }
             }
-        };
+        };*/
         
         /*self.getQuestions = function() {
             // var defferred = $q.defer();
